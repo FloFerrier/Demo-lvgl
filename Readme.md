e6300b031a420dc7b53c705fa89c5149bd6606d3
flfer <florian.ferrier@aol.fr>
2022-07-02 15:43:11 +0100

    Startup the project

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Demo-LVGL
+This project is a UI based on a raspberry pi 4 (with raspbian).
+The goal is to link this UI with an Openpill board (stm32 connected to the pi).
+## Environment
+- VSCode
+- CMake
+- LVGL
+## Project
+UI organization :
+- Temperature
+- Humidity
+- Pressure
---
+++ src/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <SDL2/SDL.h>
+
+#define SDL_MAIN_HANDLED /*To fix SDL's "undefined reference to WinMain" issue*/
+
+#include "lvgl.h"
+#include "../lv_drivers-src/display/fbdev.h"
+#include "../lv_drivers-src/sdl/sdl.h"
+
+#define DISP_BUF_SIZE (SDL_HOR_RES * 100)
+
+int main(void) {
+
+    printf("DEMO-LVGL\r\n");
+
+    /*LittlevGL init*/
+    lv_init();
+ 
+    /*Linux frame buffer device init*/
+    //fbdev_init();
+    /* Use the 'monitor' driver which creates window on PC's monitor to simulate a display*/
+    sdl_init();
+
+    /*A small buffer for LittlevGL to draw the screen's content*/
+    static lv_color_t buf[DISP_BUF_SIZE];
+
+    /*Initialize a descriptor for the buffer*/
+    static lv_disp_draw_buf_t disp_buf;
+    lv_disp_draw_buf_init(&disp_buf, buf, NULL, DISP_BUF_SIZE);
+ 
+    /*Initialize and register a display driver*/
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.draw_buf = &disp_buf;
+    disp_drv.flush_cb = sdl_display_flush;
+    disp_drv.hor_res = SDL_HOR_RES;
+    disp_drv.ver_res = SDL_VER_RES;
+    lv_disp_drv_register(&disp_drv);
+
+    /*Create a Label on the currently active screen*/
+    lv_obj_t *label =  lv_label_create(lv_scr_act());
+
+    /*Modify the Label's text*/
+    lv_label_set_text(label, "Hello world!");
+
+    /* Align the Label to the center
+     * NULL means align on parent (which is the screen now)
+     * 0, 0 at the end means an x, y offset after alignment*/
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+
+    printf("Hello World !\r\n");
+
+    /*Handle LitlevGL tasks (tickless mode)*/
+    while(1) {
+        lv_tick_inc(5);
+        lv_task_handler();
+        usleep(5000);
+    }
+
+    return 0;
+}
